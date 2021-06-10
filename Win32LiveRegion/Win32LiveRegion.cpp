184e31467fa5bae25ccdc33823cdb9ed2ece5cb1
TheDoctor <TheDoctor@Gallifrey.org>
2021-06-09 17:07:52 -0700

    Add project files.

---
+++ Win32LiveRegion/Win32LiveRegion.cpp
@@ -0,0 +1,260 @@
+// Win32LiveRegion.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "Win32LiveRegion.h"
+
+// *** LiveRegion support addition. ***
+#include <initguid.h> 
+#include "objbase.h"
+#include "uiautomation.h" 
+IAccPropServices* _pAccPropServices = NULL;
+// *** End of LiveRegion support addition. ***
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_WIN32LIVEREGION, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WIN32LIVEREGION));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WIN32LIVEREGION));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WIN32LIVEREGION);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE: Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Parse the menu selections:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// *** LiveRegion support addition. ***
+void SetLiveSettingProperty(HWND hDlg)
+{
+    HRESULT hr = S_OK;
+
+    hr = CoCreateInstance(
+        CLSID_AccPropServices,
+        nullptr,
+        CLSCTX_INPROC,
+        IID_PPV_ARGS(&_pAccPropServices));
+
+    if (SUCCEEDED(hr))
+    {
+        VARIANT var;
+        var.vt = VT_I4;
+        var.lVal = 2; // Assertive
+
+        hr = _pAccPropServices->SetHwndProp(
+            GetDlgItem(hDlg, IDC_STATUS),
+            OBJID_CLIENT,
+            CHILDID_SELF,
+            LiveSetting_Property_GUID,
+            var);
+    }
+}
+
+void ClearLiveSettingProperty(HWND hDlg)
+{
+    if (_pAccPropServices != nullptr)
+    {
+        // We only added the one property to the hwnd. 
+        MSAAPROPID props[] = { LiveSetting_Property_GUID };
+        _pAccPropServices->ClearHwndProps(
+            GetDlgItem(hDlg, IDC_STATUS),
+            OBJID_CLIENT,
+            CHILDID_SELF,
+            props,
+            ARRAYSIZE(props));
+
+        _pAccPropServices->Release();
+        _pAccPropServices = NULL;
+    }
+}
+// *** End of LiveRegion support addition. ***
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+
+        // *** LiveRegion support addition. ***
+        SetLiveSettingProperty(hDlg);
+        // *** End of LiveRegion support addition. ***
+
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+    {
+        int cmdId = LOWORD(wParam);
+
+        if (cmdId == IDOK || cmdId == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        else if (cmdId == IDC_STARTACTION)
+        {
+            SetDlgItemText(hDlg, IDC_STATUS, L"Please wait...");
+
+            // *** LiveRegion support addition. ***
+            NotifyWinEvent(
+                EVENT_OBJECT_LIVEREGIONCHANGED,
+                GetDlgItem(hDlg, IDC_STATUS),
+                OBJID_CLIENT,
+                CHILDID_SELF);
+            // *** End of LiveRegion support addition. ***
+
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    case WM_DESTROY:
+
+        // *** LiveRegion support addition. ***
+        ClearLiveSettingProperty(hDlg);
+        // *** End of LiveRegion support addition. ***
+
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
